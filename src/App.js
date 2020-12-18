98544cef35d0467e2a4a1a301cfb6110b1318638
chuckaninye <chuckaninye@gmail.com>
2020-12-17 22:18:37 -0500

    Learned how to use components and jsx expressions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
+function FirstComponent(){
+  return <h1>This is a component {"(with a jsx expression inside)."}</h1>
+}
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <FirstComponent />
   );
 }
 
