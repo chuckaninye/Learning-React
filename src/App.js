c3c52ee604e74eb91f7d4768a1a78361e1c5c1ea
chuckaninye <chuckaninye@gmail.com>
2020-12-18 20:25:21 -0500

    Learned how to use props and children props

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import { useState } from "react";
 
 
+function GreetComponent(props){
+  return <h1>Hello, {props.name} {props.children}</h1>
+}
 function App() {
-
-  const [userName, setUserName] = useState('')
-  const [password, setPassword] = useState('')
   
   return (
     <>
-    <span>{userName === 'Chuck' && password === 'Aninye' && <p>Hello Chuck Aninye</p>}</span>
-    <input type="text" value={userName} onChange={updateUserName}/>
-    <input type="password" value={password} onChange={updatePassword}/>
-    <button onClick={submitForm}>Submit Form</button>
+    <GreetComponent name="Chuck">
+      <p>Hi again!</p>
+    </GreetComponent>
     </>
   );
-
-  function updateUserName(event){
-    setUserName(event.target.value)
-    }
-  
-  function updatePassword(event){
-    setPassword(event.target.value)
-  }
-
-  function submitForm(){
-    console.log(userName, password)
-  }
 
 }
 
