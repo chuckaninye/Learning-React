cb764a60a3a670ee329c86b93592aa9fe3ee9a09
chuckaninye <chuckaninye@gmail.com>
2020-12-18 21:09:17 -0500

    Learned about routing, linking, and parameterized routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Greet from './components/Greet'
 
-
-function GreetComponent(props){
-  return <h1>Hello, {props.name} {props.children}</h1>
-}
 function App() {
   
   return (
     <>
-    <GreetComponent name="Chuck">
-      <p>Hi again!</p>
-    </GreetComponent>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/about" exact component={About}/>
+        <Route path="/greet/:name" exact component={Greet}/>
+      </Switch>
+    </Router>
     </>
   );
 
