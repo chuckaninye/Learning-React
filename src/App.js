6246be38e78947d19e68149aa4fa1f41add49477
chuckaninye <chuckaninye@gmail.com>
2020-12-18 19:38:26 -0500

    Learned how to use events and useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
 
-function FirstComponent(){
-  return <h1>This is a component {"(with a jsx expression inside)."}</h1>
-}
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
   return (
-    <FirstComponent />
+    <>
+      <button onClick={updateCounter}>Increase counter</button>
+      <h1>{counter}</h1>
+    </>
   );
+
+  function updateCounter(){
+    setCounter(counter + 1);
+  }
 }
 
 export default App;
