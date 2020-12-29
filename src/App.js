3dda36bf5aaa2a4cf8050d2009aaaadf7c13ba1d
chuckaninye <chuckaninye@gmail.com>
2020-12-29 12:16:25 -0500

    Made a grid layout using css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { createContext } from "react";
+import React from "react";
+import './index.css'
 
 const Book = () => {
   return (
-    <>
+    <article className="book">
     <Image /> 
     <Title />
     <Author />
-    </>
+    </article>
   )
 }
 
 const Image = () => {
-  return <img src="https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg"/>
+  return <img src="https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg" alt=''/>
 }
 
 const Author = () => <h3>Eric Carle</h3>
@@ -19,9 +20,13 @@ const Author = () => <h3>Eric Carle</h3>
 const Title = () => <h2>The Very Hungry Caterpillar</h2>
 function App() {
   return (
-    <>
+    <div className="app">
     <Book />
-    </>
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    </div>
   );
 
 }
