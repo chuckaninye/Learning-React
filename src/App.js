1d692e7acfadd5a5c99c3a94124094bad23c887c
chuckaninye <chuckaninye@gmail.com>
2020-12-29 20:01:31 -0500

    Created another book listing with knowledge about props

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
 import React from "react";
 import './index.css'
 
-const Book = () => {
-  return (
-    <article className="book">
-    <Image /> 
-    <Title />
-    <Author />
-    </article>
-  )
+const firstBook = {
+  title: 'The Very Hungry Caterpillar',
+  image: 'https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg',
+  author: 'Eric Carle'
 }
 
-const Image = () => {
-  return <img src="https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg" alt=''/>
+const secondBook = {
+  title: 'If Animals Kissed Good Night',
+  image: 'https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg',
+  author: 'Ann Whitford Paul'
 }
 
-const Author = () => <h3>Eric Carle</h3>
 
-const Title = () => <h2>The Very Hungry Caterpillar</h2>
 function App() {
   return (
     <div className="app">
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
+    <Book 
+    img={firstBook.image}
+    title={firstBook.title}
+    author={firstBook.author} />
+    <Book 
+    img={secondBook.image}
+    title={secondBook.title}
+    author={secondBook.author} />
     </div>
+    
   );
 
+}
+
+const Book = (props) => {
+  return (
+    <article className="book">
+    <img src={props.img}/> 
+    <h1>{props.title}</h1>
+    <h4>{props.author}</h4>
+    </article>
+  )
 }
 
 export default App;
